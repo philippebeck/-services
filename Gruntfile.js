172642a40ec3e5ceec5713ff5124dc88f47aa7d4
Philippe Beck <philippe@philippebeck.net>
2022-05-05 23:30:06 +0200

    Add export to serve

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
         banner: "/*! <%= pkg.name %> v<%= pkg.version %> | <%= pkg.homepage %> | <%= pkg.license %> License */\n\n", 
         footer: "\n/*! Author: <%= pkg.author.name %> <<%= pkg.author.email %>>\n Updated: <%= grunt.template.today('dS mmm yyyy @ h:MM:ss TT') %> */"},
       js: {
-        src: ["src/init.js", "src/axios.js", "src/string.js"],
+        src: ["src/init.js", "src/axios.js", "src/string.js", "src/export.js"],
         dest: "dist/serve.js"}
     },
     uglify: {

1c08cd744e84058ac3c5f2d09dd55a11416b73c5
Philippe Beck <philippe@philippebeck.net>
2023-01-14 23:32:13 +0400

    Switch string.js functions to : checkEmail(), checkPass() & checkUrl()

--- src/string.js
+++ src/string.js
@@ -1,121 +1,53 @@
 // ******************** STRING ******************** \\
 
 /**
- * CHECK EMPTY
- * @param {string} str 
- * @returns
- */
-function checkEmpty(str) {
-  if (str === "") {
-    alert(constants.ALERT_EMPTY);
-
-    return false;
-  }
-}
-
-/**
- * CHECK NAME
- * @param {string} str 
- * @returns 
- */
-function checkName(str) {
-  if (constants.REGEX_NAME.test(str) !== true) {
-    alert(constants.ALERT_NAME);
-
-    return false;
-  }
-}
-
-/**
- * CHECK URL
- * @param {string} str 
- * @returns 
- */
-function checkUrl(str) {
-  if (constants.REGEX_URL.test(str) !== true) {
-    alert(constants.ALERT_URL);
-
-    return false;
-  }
-}
-
-/**
  * CHECK EMAIL
- * @param {string} str 
+ * @param {string} email 
  * @returns 
  */
-function checkEmail(str) {
-  if (constants.REGEX_EMAIL.test(str) !== true) {
-    alert(constants.ALERT_EMAIL);
-
-    return false;
+function checkEmail(email) {
+  if (emailValidator.validate(email)) {
+    return true;
   }
+
+  alert(constants.ALERT_EMAIL);
+  return false;
 }
 
 /**
  * CHECK PASSWORD
- * @param {string} str 
+ * @param {string} pass 
  * @returns 
  */
-function checkPass(str) {
-  if (constants.REGEX_PASS.test(str) !== true) {
-    alert(constants.ALERT_PASS);
+function checkPass(pass) {
+  const schema = new passValidator();
 
-    return false;
+  schema
+    .is().min(constants.PASS_MIN)
+    .is().max(constants.PASS_MAX)
+    .has().uppercase()
+    .has().lowercase()
+    .has().digits(constants.PASS_INT)
+    .has().not().spaces();
+
+  if (schema.validate(pass)) {
+    return true;
   }
+
+  alert(constants.ALERT_PASS);
+  return false;
 }
 
 /**
- * CHECK STRING
- * @param {string} str
- * @param {string} type
- * @returns
- */
-function checkString(str, type) {
-  checkEmpty(str);
-
-  switch (type) {
-    case "name":
-      checkName(str);
-      break;
-    case "url":
-      checkUrl(str);
-      break;
-    case "email":
-      checkEmail(str);
-      break;
-    case "pass":
-      checkPass(str);
-      break;
-    default:
-      alert(constants.ALERT_UNKNOWN);
-
-      return false;
-      
-  }
-  return true;
-}
-
-/**
- * REWRITE STRING
- * @param {string} str
- * @param {string} type
+ * CHECK URL
+ * @param {string} url 
  * @returns 
  */
-function rewriteString(str, type) {
-  switch (type) {
-    case "name":
-      str = str.trim().charAt(0).toUpperCase() + str.trim().slice(1);
-      break;
-    case "email":
-      str = str.trim().toLowerCase();
-      break;
-    case "url":
-      str = str.trim().toLowerCase().replace(constants.REWRITE_URL, "");
-      break;
-    default:
-      str = str.trim();
+function checkUrl(url) {
+  if (validUrl.isUri(url)) {
+    return true;
   }
 
-  return str;
+  alert(constants.ALERT_URL);
+  return false;
 }

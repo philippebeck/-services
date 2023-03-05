63d90619f533ec4ee7e5e0f1782b77cf6f792e5a
Philippe Beck <philippe@philippebeck.net>
2023-03-05 11:08:12 +0400

    Rename sortItemsByCat() to getItemsByCat() & move it to getter.js

--- src/getter.js
+++ src/getter.js
@@ -31,3 +31,23 @@ function getItemName(id, items) {
   }
   return false;
 }
+
+/**
+ * GET ITEMS BY CATEGORY
+ * @param {array} items 
+ * @returns
+ */
+function getItemsByCat(items) {
+  const itemsByCat = {};
+
+  for (let item of items) {
+    if (!itemsByCat[item.cat]) {
+      itemsByCat[item.cat] = [];
+    }
+
+    itemsByCat[item.cat].push(item);
+    itemsByCat[item.cat].sort((a, b) => (a.name > b.name) ? 1 : -1);
+  }
+
+  return itemsByCat;
+}

9df0b26950ee08690c25094d5e0a05169a219bb7
Philippe Beck <philippe@philippebeck.net>
2023-04-26 02:54:10 +0400

    Remove all import packages

--- src/init.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-import axios from "axios"
-import emailValidator from "email-validator";
-import passValidator from "password-validator";
-import validUrl from "valid-url";
-
-import constants from "/constants"

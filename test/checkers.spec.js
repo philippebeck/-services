870858f5b5fde764e2cecb1d9ae2e71e0a2668bd
Philippe Beck <philippe@philippebeck.net>
2023-04-27 23:49:16 +0400

    Update checkers tests

--- test/checkers.spec.js
+++ test/checkers.spec.js
@@ -6,20 +6,6 @@ global.alert = jest.fn();
 
 describe("checkers", () => {
 
-  describe("checkEmail", () => {
-    test("should return true if email is valid", () => {
-      const email = "test@test.com";
-      const result = serve.checkEmail(email);
-      expect(result).toBe(true);
-    })
-
-    test("should return false if email is not valid", () => {
-      const email = "test@test";
-      const result = serve.checkEmail(email);
-      expect(result).toBe(false);
-    })
-  })
-
   describe("checkError", () => {
     test("should console.log if error.response doesn't exist", () => {
       const error = {};
@@ -28,30 +14,71 @@ describe("checkers", () => {
     })
   })
 
-  describe("checkNumber", () => {
+  describe("checkRange", () => {
     test("should return true if number is between min & max", () => {
       const number = 3;
-      const result = serve.checkNumber(number);
+      const message = "Number is not between";
+      const min = 2;
+      const max = 4;
+      const result = serve.checkRange(number, message, min, max);
       expect(result).toBe(true);
     })
 
     test("should return false if number is not between min & max", () => {
       const number = 6;
-      const result = serve.checkNumber(number);
+      const message = "Number is not between";
+      const min = 2;
+      const max = 4;
+      const result = serve.checkRange(number, message, min, max);
+      expect(result).toBe(false);
+    })
+
+    test("should return true if string is between min & max", () => {
+      const string = "test";
+      const message = "String is not between";
+      const result = serve.checkRange(string, message);
+      expect(result).toBe(true);
+    })
+
+    test("should return false if string is not between min & max", () => {
+      const string = "a";
+      const message = "String is not between";
+      const result = serve.checkRange(string, message);
       expect(result).toBe(false);
     })
   })
 
-  describe("checkPass", () => {
+  describe("checkRegex", () => {
+    
+    test("should return true if email is valid", () => {
+      const email = "test@test.com";
+      const message = "Email is not valid";
+      const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+      const result = serve.checkRegex(email, message, regex);
+      expect(result).toBe(true);
+    })
+
+    test("should return false if email is not valid", () => {
+      const email = "test@test";
+      const message = "Email is not valid";
+      const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+      const result = serve.checkRegex(email, message, regex);
+      expect(result).toBe(false);
+    })
+
     test("should return true if pass is valid", () => {
       const pass = "Test1234";
-      const result = serve.checkPass(pass);
+      const message = "Password is not valid";
+      const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/g;
+      const result = serve.checkRegex(pass, message, regex);
       expect(result).toBe(true);
     })
 
     test("should return false if pass is not valid", () => {
       const pass = "test";
-      const result = serve.checkPass(pass);
+      const message = "Password is not valid";
+      const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/g;
+      const result = serve.checkRegex(pass, message, regex);
       expect(result).toBe(false);
     })
   })
@@ -67,28 +94,6 @@ describe("checkers", () => {
       const role = "test";
       const result = serve.checkRole(role);
       expect(result).toBe(false);
-    })
-  })
-
-  describe("checkString", () => {
-    test("should return true if string is between min & max", () => {
-      const string = "test";
-      const result = serve.checkString(string);
-      expect(result).toBe(true);
-    })
-
-    test("should return false if string is not between min & max", () => {
-      const string = "a";
-      const result = serve.checkString(string);
-      expect(result).toBe(false);
-    })
-  })
-
-  describe("checkUrl", () => {
-    test("should return true if url is valid", () => {
-      const url = "https://test.com";
-      const result = serve.checkUrl(url);
-      expect(result).toBe(true);
     })
   })
 })

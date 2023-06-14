e3783b90f702bb5535db0fe2c8ff21a27a34110e
Philippe Beck <philippe@philippebeck.net>
2023-06-15 01:04:42 +0400

    Update fetchers tests

--- test/fetchers.spec.js
+++ test/fetchers.spec.js
@@ -1,46 +1,133 @@
-// ******************** FETCHERS ******************** \\
+// ! **************************************** FETCHERS ****************************************
+// ! ******************************************************************************************
 
-import * as serve from "../dist/serve"
+import { fetchGet, fetchSet } from "../src/fetchers"
 
-describe("fetchers", () => {
+/**
+ * ? FETCH GET
+ */
+describe('fetchGet()', () => {
+  const url = 'https://example.com/data';
 
-  /**
-   * FETCH GET
-   */
-  describe("fetchGet", () => {
-    test("should fetch an url", async () => {
-      let url = `https://api.philippebeck.net/products/6407595d656f6b26ddc6bbf6`
+  test('should fetch data from a given URL with application/json content type', async () => {
+    const data = { name: 'John', age: 30 };
 
-      fetch.mockResponseOnce(JSON.stringify(
-        {
-          name: "1h",
-          alt: "1h",
-        }
-      ));
-      const res = await serve.fetchGet(url);
-      expect(res).toEqual('{"name":"1h","alt":"1h"}');
-      expect(fetch.mock.calls.length).toEqual(1);
-    })
+    const mockedResponse = new Response(JSON.stringify(data), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce(mockedResponse);
+    const result = await fetchGet(url);
+
+    expect(result).toEqual(data);
+    expect(global.fetch).toHaveBeenCalledWith(url);
   })
 
-  /**
-   * FETCH SET
-   */
-  // describe("fetchSet", () => {
-  //   test("should fetch an url with a body", async () => {
-  //     let url = "https://api.philippebeck.net/auth";
-  //     let options = {
-  //       method: "POST",
-  //       mode: "cors",
-  //       body: JSON.stringify({
-  //         email: "test@test.com",
-  //         pass: "testTEST1234"
-  //       })
-  //     };
+  // test('should fetch data from a given URL with multipart/form-data content type', async () => {
+  //   const data = new FormData();
+  //   data.append('name', 'John');
 
-  //     return serve.fetchSet(url, options).then(result => {
-  //       expect(result).toHaveBeenCalledWith(url, options);
-  //     })
-  //   })
+  //   const mockedResponse = new Response(data, {
+  //     status: 200,
+  //     headers: { 'Content-Type': 'multipart/form-data' },
+  //   });
+
+  //   jest.spyOn(global, 'fetch').mockResolvedValueOnce(mockedResponse);
+  //   const result = await fetchGet(url);
+
+  //   expect(result).toEqual(data);
+  //   expect(global.fetch).toHaveBeenCalledWith(url);
   // })
+
+  test('should fetch data from a given URL with text/html content type', async () => {
+    const data = '<html><body><h1>Hello World!</h1></body></html>';
+
+    const mockedResponse = new Response(data, {
+      status: 200,
+      headers: { 'Content-Type': 'text/html' },
+    });
+
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce(mockedResponse);
+    const result = await fetchGet(url);
+
+    expect(result).toEqual(data);
+    expect(global.fetch).toHaveBeenCalledWith(url);
+  })
+
+  test('should fetch data from a given URL with text/plain content type', async () => {
+    const data = 'Hello World!';
+
+    const mockedResponse = new Response(data, {
+      status: 200,
+      headers: { 'Content-Type': 'text/plain' },
+    });
+
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce(mockedResponse);
+    const result = await fetchGet(url);
+
+    expect(result).toEqual(data);
+    expect(global.fetch).toHaveBeenCalledWith(url);
+  })
+
+  test('should return the response body for an unknown content type', async () => {
+    const mockedResponse = new Response('Hello World!', {
+      status: 200,
+      headers: { 'Content-Type': 'application/octet-stream' },
+    });
+
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce(mockedResponse);
+    const result = await fetchGet(url);
+
+    expect(result).toEqual(mockedResponse.body);
+    expect(global.fetch).toHaveBeenCalledWith(url);
+  })
+
+  test('should throw an error if the response status is not ok', async () => {
+    const mockedResponse = new Response('Not Found', {
+      status: 404,
+      headers: { 'Content-Type': 'text/plain' },
+    });
+
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce(mockedResponse);
+
+    await expect(fetchGet(url)).rejects.toThrow(Error);
+    expect(global.fetch).toHaveBeenCalledWith(url);
+  })
+})
+
+/**
+ * ? FETCH SET
+ */
+describe("fetchSet()", () => {
+
+  test("should fetch data from a given URL using provided options", async () => {
+    const url = "http://example.com";
+    const options = { method: "GET" };
+    const data = { message: "Hello, world!" };
+    const response = new Response(JSON.stringify(data), { status: 200 });
+
+    const originalFetch = globalThis.fetch;
+    globalThis.fetch = jest.fn(() => Promise.resolve(response));
+
+    const result = await fetchSet(url, options);
+
+    expect(result).toEqual(data);
+    expect(globalThis.fetch).toHaveBeenCalledWith(url, options);
+
+    globalThis.fetch = originalFetch;
+  })
+
+  test("should throw an error if the response is not OK", async () => {
+    const url = "http://example.com";
+    const options = { method: "GET" };
+    const response = new Response("Not found", { status: 404 });
+
+    const originalFetch = globalThis.fetch;
+    globalThis.fetch = jest.fn(() => Promise.resolve(response));
+
+    await expect(fetchSet(url, options)).rejects.toThrowError();
+
+    globalThis.fetch = originalFetch;
+  })
 })

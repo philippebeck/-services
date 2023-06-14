dcd48fc6f55d1a6df8633337e3f46d763456e6d1
Philippe Beck <philippe@philippebeck.net>
2023-06-14 22:56:07 +0400

    Update checkers tests

--- test/checkers.spec.js
+++ test/checkers.spec.js
@@ -1,222 +1,98 @@
-// ******************** CHECKERS ******************** \\
+// ! **************************************** CHECKERS TESTS ****************************************
 
-import * as serve from "../dist/serve"
+import { checkRange, checkRegex, checkRole } from "../src/checkers"
 
 global.alert = jest.fn();
-global.console.log = jest.fn();
 
-describe("checkers", () => {
+/**
+ * ? CHECK RANGE
+ */
+describe("checkRange()", () => {
+  const msg = "Value out of range";
 
-  /**
-   * CHECK ERROR
-   */
-  describe("checkError", () => {
-    test("should alert if error.response exist", () => {
-      const error = {
-        response: {
-          data: {
-            message: "test"
-          }
-        }
-      };
-      serve.checkError(error);
-      expect(alert).toHaveBeenCalledWith(error.response.data.message);
-    })
-
-    test("should console.log if error.response doesn't exist", () => {
-      const error = {};
-      serve.checkError(error);
-      expect(console.log).toHaveBeenCalledWith(error);
-    })
+  test("should return true if value is within the specified range", () => {
+    expect(checkRange(2, msg)).toBe(true);
+    expect(checkRange(50, msg)).toBe(true);
+    expect(checkRange("aA", msg)).toBe(true);
+    expect(checkRange("abcdefghijklmnopqrstuvwxyz", msg)).toBe(true);
   })
 
-  /**
-   * CHECK ID
-   */
-  describe("checkId", () => {
-    test("should return true if id is inside array", () => {
-      const id = "1";
-      const array = ["1", "2", "3"];
-      const result = serve.checkId(id, array);
-      expect(result).toBe(true);
-    })
-
-    test("should return false if id is not inside array", () => {
-      const id = "4";
-      const array = ["1", "2", "3"];
-      const result = serve.checkId(id, array);
-      expect(result).toBe(false);
-    })
+  test("should return false if value is not within the specified range", () => {
+    expect(checkRange(1, msg)).toBe(false);
+    expect(checkRange(51, msg)).toBe(false);
+    expect(checkRange("", msg)).toBe(false);
+    expect(checkRange("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", msg)).toBe(false);
   })
 
-  /**
-   * CHECK RANGE FOR NUMBER
-   */
-  describe("checkRange for number", () => {
-    const message = "Number is not between";
+  test("should display the correct message if value is not within the specified range", () => {
     const min = 2;
-    const max = 4;
+    const max = 50;
 
-    test("should return true if number is between min & max", () => {
-      const result = serve.checkRange(3, message, min, max);
-      expect(result).toBe(true);
-    })
+    checkRange(1, msg, min, max);
+    expect(alert).toHaveBeenCalledWith(`${msg} ${min} & ${max}`);
 
-    test("should return false if number is not between min & max", () => {
-      const result = serve.checkRange(6, message, min, max);
-      expect(result).toBe(false);
-    })
+    checkRange(51, msg, min, max);
+    expect(alert).toHaveBeenCalledWith(`${msg} ${min} & ${max}`);
+
+    checkRange("", msg, min, max);
+    expect(alert).toHaveBeenCalledWith(`${msg} ${min} & ${max}`);
+
+    checkRange("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", msg, min, max);
+    expect(alert).toHaveBeenCalledWith(`${msg} ${min} & ${max}`);
+  })
+})
+
+/**
+ * ? CHECK REGEX
+ */
+describe("checkRegex()", () => {
+
+  test("value matches regex", () => {
+    const value = "1234";
+    const regex = /^\d+$/;
+    const msg = "Value must contain only digits";
+
+    const result = checkRegex(value, msg, regex);
+    expect(result).toBe(true);
   })
 
-  /**
-   * CHECK RANGE FOR STRING
-   */
-  describe("checkRange for string", () => {
-    const message = "String is not between";
+  test("value does not match regex", () => {
+    const value = "12a4";
+    const regex = /^\d+$/;
+    const msg = "Value must contain only digits";
 
-    test("should return true if string is between min & max", () => {
-      const result = serve.checkRange("test", message);
-      expect(result).toBe(true);
-    })
+    const result = checkRegex(value, msg, regex);
+    expect(alert).toHaveBeenCalledWith(msg);
+    expect(result).toBe(false);
+  })
+})
 
-    test("should return false if string is not between min & max", () => {
-      const result = serve.checkRange("a", message);
-      expect(result).toBe(false);
-    })
+/**
+ * ? CHECK ROLE
+ */
+describe("checkRole()", () => {
+
+  test("returns true if userRole is admin & role is admin", () => {
+    expect(checkRole("admin", "admin")).toBe(true);
   })
 
-  /**
-   * CHECK REGEX FOR EMAIL
-   */
-  describe("checkRegex for email", () => {
-    const message = "Email is not valid";
-    const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-    
-    test("should return true if email is valid", () => {
-      const result = serve.checkRegex("test@test.com", message, regex);
-      expect(result).toBe(true);
-    })
-
-    test("should return false if email is not valid", () => {
-      const result = serve.checkRegex("test@test", message, regex);
-      expect(result).toBe(false);
-    })
+  test("returns false if userRole is editor & role is admin", () => {
+    expect(checkRole("editor", "admin")).toBe(false);
   })
 
-  /**
-   * CHECK REGEX FOR PASSWORD
-   */
-  describe("checkRegex for password", () => {
-    const message = "Password is not valid";
-    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/g;
-
-    test("should return true if pass is valid", () => {
-      const result = serve.checkRegex("Test1234", message, regex);
-      expect(result).toBe(true);
-    })
-
-    test("should return false if pass is not valid", () => {
-      const result = serve.checkRegex("test", message, regex);
-      expect(result).toBe(false);
-    })
+  test("returns true if userRole is editor & role is editor", () => {
+    expect(checkRole("editor", "editor")).toBe(true);
   })
 
-  /**
-   * CHECK REGEX FOR URL
-   */
-  describe("checkRegex for url", () => {
-    const message = "Url is not valid";
-    const regex = /^(http|https):\/\/[^ "]+$/g;
-
-    test("should return true if url is valid", () => {
-      const result = serve.checkRegex("https://test.com", message, regex);
-      expect(result).toBe(true);
-    })
-
-    test("should return false if url is not valid", () => {
-      const result = serve.checkRegex("test", message, regex);
-      expect(result).toBe(false);
-    })
+  test("returns false if userRole is user & role is editor", () => {
+    expect(checkRole("user", "editor")).toBe(false);
   })
 
-  /**
-   * CHECK ROLE FOR ADMIN
-   */
-  describe("checkRole for admin", () => {
-    const role = "admin";
-
-    test("should return true if user is admin", () => {
-      const result = serve.checkRole("admin", role);
-      expect(result).toBe(true);
-    })
-
-    test("should return false if user is editor", () => {
-      const result = serve.checkRole("editor", role);
-      expect(result).toBe(false);
-    })
-
-    test("should return false if user is user", () => {
-      const result = serve.checkRole("user", role);
-      expect(result).toBe(false);
-    })
-
-    test("should return false if user is test", () => {
-      const result = serve.checkRole("test", role);
-      expect(result).toBe(false);
-    })
+  test("returns true if userRole is user & role is user", () => {
+    expect(checkRole("user", "user")).toBe(true);
   })
 
-  /**
-   * CHECK ROLE FOR EDITOR
-   */
-  describe("checkRole for editor", () => {
-    const role = "editor";
-
-    test("should return true if user is admin", () => {
-      const result = serve.checkRole("admin", role);
-      expect(result).toBe(true);
-    })
-
-    test("should return true if user is editor", () => {
-      const result = serve.checkRole("editor", role);
-      expect(result).toBe(true);
-    })
-
-    test("should return false if user is user", () => {
-      const result = serve.checkRole("user", role);
-      expect(result).toBe(false);
-    })
-
-    test("should return false if user is test", () => {
-      const result = serve.checkRole("test", role);
-      expect(result).toBe(false);
-    })
-  })
-
-  /**
-   * CHECK ROLE FOR USER
-   */
-  describe("checkRole for user", () => {
-    const role = "user";
-
-    test("should return true if user is admin", () => {
-      const result = serve.checkRole("admin", role);
-      expect(result).toBe(true);
-    })
-
-    test("should return true if user is editor", () => {
-      const result = serve.checkRole("editor", role);
-      expect(result).toBe(true);
-    })
-
-    test("should return true if user is user", () => {
-      const result = serve.checkRole("user", role);
-      expect(result).toBe(true);
-    })
-
-    test("should return false if user is test", () => {
-      const result = serve.checkRole("test", role);
-      expect(result).toBe(false);
-    })
+  test("returns false if userRole is test & role is user", () => {
+    expect(checkRole("test", "user")).toBe(false);
   })
 })

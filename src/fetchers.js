c9b4125e6d42bca6f00b3653542bc36f2ba0a8a3
Philippe Beck <philippe@philippebeck.net>
2023-11-22 09:41:17 +0400

    Add Axios again

--- src/fetchers.js
+++ src/fetchers.js
@@ -2,12 +2,96 @@
 // ! ******************************************************************************************
 
 /**
- * ? FETCH GET
- * * An asynchronous function that fetches data from a given URL & returns the response based on the content type.
+ * ? SET AXIOS
+ * * Sets the base URL and headers for Axios requests
  *
- * @param {string} url - The URL to fetch data from.
- * @throws {Error} Throws an error if the response status is not ok.
- * @return {Promise} Returns a Promise that resolves with the appropriate data based on the content type of the response.
+ * @param {string} url - The base URL for Axios requests
+ * @param {string} type - The content type for Axios requests
+ * @param {string|null} token - The authorization token for Axios requests
+ */
+export function setAxios(url, type, token = null) {
+  axios.defaults.baseURL = url;
+  axios.defaults.headers.post["Content-Type"] = type;
+  
+  if (token) {
+    axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+  }
+}
+
+/**
+ * ? POST DATA
+ * * Sends a POST request to the specified URL with the provided data
+ *
+ * @param {string} url - The URL to send the POST request to
+ * @param {string} type - The type of data being sent
+ * @param {object} data - The data to send in the request body
+ * @param {string|null} [token=null] - An optional authentication token
+ * @return {Promise} A Promise that resolves to the response data
+ */
+export async function postData(url, type, data, token = null) {
+  setAxios(url, type, token);
+  const response = await axios.post(url, data);
+
+  return response.data;
+}
+
+/**
+ * ? GET DATA
+ * * Retrieves data from a specified URL using a GET request
+ *
+ * @param {string} url - The URL to fetch the data from
+ * @param {string} type - The type of data being fetched
+ * @param {string|null} [token=null] - An optional authentication token
+ * @return {Promise} - A promise that resolves with the fetched data
+ */
+export async function getData(url, type, token = null) {
+  setAxios(url, type, token);
+  const response = await axios.get(url);
+
+  return response.data;
+}
+
+/**
+ * ? PUT DATA
+ * * Sends a PUT request to the specified URL with the provided data
+ *
+ * @param {string} url - The URL to send the request to
+ * @param {string} type - The type of data being sent
+ * @param {any} data - The data to send in the request body
+ * @param {string|null} [token=null] - An optional authentication token
+ * @return {Promise} A promise that resolves to the response data
+ */
+export async function putData(url, type, data, token = null) {
+  setAxios(url, type, token);
+  const response = await axios.put(url, data);
+
+  return response.data;
+}
+
+/**
+ * ? DELETE DATA
+ * * Deletes data from the given URL using the specified HTTP method & optional token
+ *
+ * @param {string} url - The URL to send the delete request to
+ * @param {string} type - The HTTP method to use for the delete request
+ * @param {string|null} [token=null] - An optional authentication token
+ * @return {Promise} - A Promise that resolves with the response data
+ */
+export async function deleteData(url, type, token = null) {
+  setAxios(url, type, token);
+  const response = await axios.delete(url);
+
+  return response.data;
+}
+
+/**
+ * ? FETCH GET
+ * * An asynchronous function that fetches data from a given URL
+ * * And returns the response based on the content type
+ *
+ * @param {string} url - The URL to fetch data from
+ * @throws {Error} Throws an error if the response status is not ok
+ * @return {Promise} Returns a Promise that resolves the fetched data
  */
 export async function fetchGet(url) {
   const response = await fetch(url);
@@ -32,12 +116,12 @@ export async function fetchGet(url) {
 
 /**
  * ? FETCH SET
- * * Asynchronously fetches data from a given URL using the provided options object.
+ * * Asynchronously fetches data from a given URL using the provided options object
  *
- * @param {string} url - The URL to fetch data from.
- * @param {object} options - An options object that is passed to the fetch function.
- * @throws {Error} If the response is not OK.
- * @return {Promise<object>} A promise that resolves to a JSON object representing the fetched data.
+ * @param {string} url - The URL to fetch data from
+ * @param {object} options - An options object that is passed to the fetch function
+ * @throws {Error} If the response is not OK
+ * @return {Promise<object>} A promise that resolves to a JSON object representing the fetched data
  */
 export async function fetchSet(url, options) {
   const response = await fetch(url, options);

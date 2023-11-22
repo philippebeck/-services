8f9aeaca2454c3a33757754706f98f8fe34f56d7
Philippe Beck <philippe@philippebeck.net>
2023-11-22 09:42:14 +0400

    Remove Twitter Cards

--- src/setters.js
+++ src/setters.js
@@ -12,51 +12,37 @@ export function setError(error) {
 
 /**
  * ? SET GLOBAL META
- * * Sets the global metadata of the page including language, favicon & Twitter creator
+ * * Sets the metadata of the website including language & favicon
  *
- * @param {string} creator - The Twitter creator handle to set in the metadata
- * @param {string} [icon="img/favicon.ico"] - The path to the favicon to set in the metadata
  * @param {string} [lang="en"] - The language code to set in the metadata
+ * @param {string} [icon="img/favicon.ico"] - The path to the favicon to set in the metadata
  */
-export function setGlobalMeta(creator, icon = "img/favicon.ico", lang = "en") {
-  const iconElt     = document.querySelector('[rel="icon"]');
-  const creatorElt  = document.querySelector('[name="twitter:creator"]');
+export function setGlobalMeta(lang = "en", icon = "img/favicon.ico") {
+  const iconElt = document.querySelector('[rel="icon"]');
 
   document.documentElement.lang = lang;
   if (iconElt) iconElt.href = icon;
-
-  if (creatorElt) {
-    if (creator === undefined) {
-      creatorElt.remove();
-    } else {
-      creatorElt.content = creator;
-    }
-  }
 }
 
 /**
  * ? SET META
  * * Sets the metadata of the page including title, description, url & image
  *
- * @param {string} title - the new title to set
- * @param {string} description - The new description to set
- * @param {string} url - The new URL to set
- * @param {string} [image=""] - The new image to set
+ * @param {string} title - the title to set
+ * @param {string} description - The description to set
+ * @param {string} url - The URL to set
+ * @param {string|null} [image] - The image to set
  */
-export function setMeta(title, description, url, image = "") {
-  const descriptionTags = '[name="description"], [property="og:description"], [name="twitter:description"]';
-  const titleTags       = '[property="og:title"], [name="twitter:title"]';
-  const urlTags         = '[property="og:url"], [name="twitter:site"]';
+export function setMeta(title, description, url, image = null) {
+  const descriptionTags = '[name="description"], [property="og:description"]';
 
   document.querySelector('title').innerText         = title;
   document.querySelector('[rel="canonical"]').href  = url;
 
-  document.querySelectorAll(titleTags).forEach(titleTag => titleTag.setAttribute("content", title));
-  document.querySelectorAll(descriptionTags).forEach(descriptionTag => descriptionTag.setAttribute("content", description));
-  document.querySelectorAll(urlTags).forEach(urlTag => urlTag.setAttribute("content", url));
+  document.querySelector('[property="og:title"]').setAttribute("content", title);
+  document.querySelector('[property="og:url"]').setAttribute("content", url);
 
-  if (image !== "") {
-    const imgTags = '[property="og:image"], [name="twitter:image"]';
-    document.querySelectorAll(imgTags).forEach(imgTag => imgTag.setAttribute("content", image));
-  }
+  document.querySelectorAll(descriptionTags).forEach(descriptionTag => descriptionTag.setAttribute("content", description));
+
+  if (image) document.querySelector('[property="og:image"]').setAttribute("content", image);
 }

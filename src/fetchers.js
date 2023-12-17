ebdb4113c03939cd8a1765834d4cc19d63ae4bc4
Philippe Beck <philippe@philippebeck.net>
2023-12-17 15:57:36 +0400

    Refactoring setAxios + blank lines to fetchers

--- src/fetchers.js
+++ src/fetchers.js
@@ -8,23 +8,19 @@ import axios from "axios";
  * ? SET AXIOS
  * * Sets Axios default headers
  * * with an optional token & an optional content-type
- *
  * @param {string|null} [token=null] - An optional authentication token
  * @param {string} [type="multipart/form-data"] - An optional Content-Type
  */
 export function setAxios(token = null, type = "multipart/form-data") {
   axios.defaults.headers.post["Content-Type"] = type;
   
-  if (token) {
-    axios.defaults.headers.common["Authorization"] = "Bearer " + token;
-  }
+  if (token) axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 }
 
 /**
  * ? POST DATA
  * * Sends a POST request to the specified URL
  * * with the provided data, an optional token & an optional content-type
- *
  * @param {string} url - The URL to send the POST request to
  * @param {object} data - The data to send in the request body
  * @param {string|null} [token=null] - An optional authentication token
@@ -42,7 +38,6 @@ export async function postData(url, data, token = null, type = "multipart/form-d
  * ? GET DATA
  * * Sends a GET request to the specified URL
  * * with an optional token & an optional content-type
- *
  * @param {string} url - The URL to send the GET request to
  * @param {string|null} [token=null] - An optional authentication token
  * @param {string} [type="multipart/form-data"] - An optional Content-Type
@@ -59,7 +54,6 @@ export async function getData(url, token = null, type = "multipart/form-data") {
  * ? PUT DATA
  * * Sends a PUT request to the specified URL
  * * with the provided data, an optional token & an optional content-type
- *
  * @param {string} url - The URL to send the PUT request to
  * @param {object} data - The data to send in the request body
  * @param {string|null} [token=null] - An optional authentication token
@@ -77,7 +71,6 @@ export async function putData(url, data, token = null, type = "multipart/form-da
  * ? DELETE DATA
  * * Sends a DELETE request to the specified URL
  * * with an optional token & an optional content-type
- *
  * @param {string} url - The URL to send the DELETE request to
  * @param {string|null} [token=null] - An optional authentication token
  * @param {string} [type="multipart/form-data"] - An optional Content-Type
